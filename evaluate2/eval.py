ff263116a9bd46bc773fd575dac3866b45fc80a2
matyasosvath <matyasosv@gmail.com>
2025-10-17 11:10:03 +0200

    add mlflow integration

--- evaluate2/eval.py
+++ evaluate2/eval.py
@@ -33,34 +33,21 @@ def evaluate(args) -> None:
 
     for task_name in args.tasks:
 
-        logging.info(f"Started evaluation on {task_name}.")
+        logging.info(f"Started evaluation for {task_name}.")
         task_start_time = time.time()
 
         dataset, tokenizer = preprocess(args, task_name)
         results = fine_tune(args, dataset, tokenizer, task_name)
         score_results[task_name] = results
 
-        logging.info(
-            f"Task {task_name} took {time.time() - task_start_time:.3f} seconds on {args.device}."
-        )
+        logging.info(f"Task {task_name} took {time.time() - task_start_time:.3f} seconds.")
 
-    logging.info(
-        f"Evaluation took {time.time() - eval_start_time:.3f} seconds "
-        f"on {args.device} for tasks: {', '.join(args.tasks)}."
-    )
+    logging.info(f"Evaluation took {time.time() - eval_start_time:.3f} seconds for tasks: {', '.join(args.tasks)}.")
 
-    if args.save_results:
-        current_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        helper.save_json(
-            score_results, ".", f"hulu-{args.model_name.replace('/', '-')}-results-{current_time}.json"
-        )
+    helper.save_results(args, score_results)
 
 
 def compute_metrics(eval_pred):
-
-    print("Eval predictions shape")
-    print(eval_pred)
-    print(eval_pred.predictions)
 
     logits = torch.tensor(eval_pred.predictions)
     preds = torch.argmax(logits, dim=1)
@@ -68,8 +55,6 @@ def compute_metrics(eval_pred):
 
     preds_np = preds.cpu().numpy()
     labels_np = labels.cpu().numpy()
-
-    print("Labels:" , labels_np)
 
     accuracy = accuracy_score(labels_np, preds_np)
     balanced_acc = balanced_accuracy_score(labels_np, preds_np)
@@ -115,11 +100,11 @@ def fine_tune(args, dataset, tokenizer, task_name: str) -> dict:
         model_kwargs = {"num_labels": 3} if task_name in [constants.CB, constants.SST] else {"num_labels": 2}
         model = AutoModelForSequenceClassification.from_pretrained(args.model_name, **model_kwargs)
 
-    parameters = helper.read_json(args.parameters)
+    parameters = helper.read_json(args.parameters_path)
 
     training_args = TrainingArguments(
-        output_dir=f"{args.save_results}{task_name}",
-        logging_dir=f"{args.save_results}{task_name}/logs",
+        output_dir=f"{args.save_results_path}{task_name}",
+        logging_dir=f"{args.save_results_path}{task_name}/logs",
         run_name=f"hulu-{task_name}-{args.model_name.replace('/', '-')}",
         **parameters
     )
@@ -129,8 +114,6 @@ def fine_tune(args, dataset, tokenizer, task_name: str) -> dict:
         args=training_args,
         train_dataset=dataset["train"],
         eval_dataset=dataset["validation"],
-        # train_dataset=small_train,
-        # eval_dataset=small_val,
         data_collator=data_collator,
         compute_metrics=compute_metrics
     )
@@ -140,6 +123,7 @@ def fine_tune(args, dataset, tokenizer, task_name: str) -> dict:
             trainer.train()
     else:
         trainer.train()
+
     if args.eval_test:
-        results = trainer.evaluate(eval_dataset=dataset["test"])
+        results = trainer.evaluate(eval_dataset=dataset["validation"]) # TODO replace with test set
     return results
--- evaluate2/helper.py
+++ evaluate2/helper.py
@@ -1,7 +1,9 @@
+import os
 import json
-import logging
 import random
 import pathlib
+from datetime import datetime
+
 import torch
 
 
@@ -9,13 +11,6 @@ def set_seeds(args) -> None:
     random.seed(args.seed)
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
-
-
-def set_device(args) -> None:
-    use_cuda = args.use_cuda and torch.cuda.is_available()
-    device = torch.device("cuda") if use_cuda else torch.device("cpu")
-    logging.info(f"Using device: {device}")
-    args.device = device
 
 
 def read_file(file_path, readlines: bool = False):
@@ -48,14 +43,51 @@ def save_json(data, dir_path, file_name: str) -> None:
     except OSError as e:
         raise OSError(f"Could not save file: {file_path}") from e
 
-def setup_reporting(args) -> None:
 
-    if args.report_to.lower() != 'wandb':
-        raise ValueError(f"Unsupported reporting destination: {args.report_to}")
+def setup_reporting(args) -> None:
+    print("Setting up reporting...")
+
+    if args.report_to.lower() == 'wandb':
+        try:
+            import wandb
+        except ImportError:
+            raise ImportError("wandb is not installed. Please install it to use wandb reporting.")
+        raise ValueError(f"Currently unsupported reporting destination: {args.report_to}")
 
     elif args.report_to.lower() == 'mlflow':
-        import mlflow
-        mlflow.set_tracking_uri(args.report_uri)
-        mlflow.set_experiment(args.experiment_name)
 
+        try:
+            import mlflow
+        except ImportError:
+            raise ImportError("mlflow is not installed. Please install it to use mlflow reporting.")
+
+        run_name = args.run_name or os.getenv('MLFLOW_RUN_NAME')
+        experiment_name = args.experiment_name or os.getenv('MLFLOW_EXPERIMENT_NAME')
+        report_uri = args.report_uri or os.getenv('MLFLOW_TRACKING_URI')
+        mlflow_username = os.getenv('MLFLOW_TRACKING_USERNAME')
+        mlflow_password = os.getenv('MLFLOW_TRACKING_PASSWORD')
+
+        assert report_uri is not None, "MLflow reporting requires a report URI to be set."
+        assert run_name is not None, "MLflow reporting requires a run name to be set."
+        assert mlflow_username is not None, "MLflow reporting requires a username env var to be set."
+        assert mlflow_password is not None, "MLflow reporting requires a password env var to be set."
+
+        mlflow.set_tracking_uri(report_uri)
+        mlflow.set_experiment(experiment_name)
         args.reporting = True
+
+    else:
+        args.reporting = False
+
+
+def save_results(args, results) -> None:
+
+    if not args.save_results_path:
+        return None
+
+    dir_path = pathlib.Path(args.save_results_path)
+    current_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    eval_name = args.model_name.replace('/', '-') # e.g. google/bert-base-uncased -> google-bert-base-uncased
+    file_name = f"hulu-{eval_name}-results-{current_time}.json"
+
+    save_json(results, dir_path, file_name)
